1e69ad36694fe4e83e5a1c7fadbfecb23bcd8ca3
chiel <chiel.fernhout@postnl.nl>
2021-12-02 08:02:00 +0100

    Day 2 part 1

--- days/02/index.ts
+++ days/02/index.ts
@@ -1,1 +1,34 @@
-console.log('Hello World!')
+import * as fs from 'fs';
+import * as path from 'path';
+
+function parseData(command: string) {
+  const [direction, magnitude] = command.split(' ')
+  return [direction, magnitude]
+}
+
+function readData() {
+  const filePath = path.join(__dirname.replace('_build', 'days'), 'data.txt');
+  const file = fs.readFileSync(filePath,'utf8').split('\n');
+  return file
+}
+
+function getPosition(data: Array<string>) {
+  var horPosition = 0
+  var verPosition = 0
+  for (var command of data) {
+    var [direction, magnitude] = parseData(command)
+    if (direction === 'up') {
+      verPosition -= Number(magnitude)
+    }
+    if (direction === 'down') {
+      verPosition += Number(magnitude)
+    }
+    if (direction === 'forward') {
+      horPosition += Number(magnitude)
+    }
+  }
+  return horPosition * verPosition
+}
+
+console.log(getPosition(readData()))
+// console.log(parseData('up 3'))
